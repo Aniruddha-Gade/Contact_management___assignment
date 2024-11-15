9db754b5c72094e9abdac50b9cf878b59020ac20
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-11-15 15:47:57 +0530

    setup frontend with Vite + react and Tailwind CSS

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+  return (
+   <div>
+    <h1 class="text-3xl text-red-600 text-center font-bold underline">
+    Hello world!
+  </h1> 
+   </div>
+  )
+}
+
+export default App
