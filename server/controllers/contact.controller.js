18c5ea8e5f2bb72b3584c661d40ffc538d6a76a9
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-11-15 15:04:32 +0530

    controller to update conatct by ID

--- server/controllers/contact.controller.js
+++ server/controllers/contact.controller.js
@@ -14,6 +14,7 @@ export const createContact = async (req, res) => {
         // validate data
         if (!firstName || !lastName || !email || !phoneNumber || !company || !jobTitle) {
             return res.status(400).json({
+                succes: false,
                 message: "firstName, lastName, email, phoneNumber, company, jobTitle all fields are required",
             });
         }
@@ -26,12 +27,14 @@ export const createContact = async (req, res) => {
 
         // return success message
         res.status(201).json({
+            succes: true,
             message: "New Contact created successfully",
             contact
         });
     } catch (error) {
         console.log("Error while creating new contact => ", error)
         res.status(400).json({
+            succes: false,
             message: 'Error while creating new contact',
             error: error.message
         });
@@ -49,13 +52,56 @@ export const getAllContacts = async (req, res) => {
 
         // return success message
         res.status(201).json({
+            succes: true,
             message: "All Contacts fetched successfully",
             contacts
         });
     } catch (error) {
         console.log("Error while fetching all contacts => ", error)
         res.status(500).json({
+            succes: false,
             message: 'Error while fetching all contacts',
+            error: error.message
+        });
+    }
+};
+
+
+
+
+// ========================= UPDATE CONTACT WITH ID ========================= 
+export const updateContact = async (req, res) => {
+    try {
+        const { id } = req.params;
+        // validate data
+        if (!id) {
+            return res.status(400).json({
+                message: "conatct ID is required to update contact",
+            });
+        }
+
+        const updatedData = req.body;
+
+        const contact = await ContactModel.findByIdAndUpdate(id,
+            updatedData,
+            { new: true }
+        );
+
+        if (!contact) return res.status(404).json({
+            succes: true,
+            message: 'Contact not found by ID',
+        });
+
+        res.status(200).json({
+            succes:true,
+            message:"Conatct updated successfully",
+            contact
+        });
+    } catch (error) {
+        console.log("Error while updating contact by Id => ", error)
+        res.status(500).json({
+            succes: false,
+            message: 'Error while updating contact by Id',
             error: error.message
         });
     }
