5cfc3da7aff8695e7d8c54dd787b999001dad89b
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-11-15 15:17:45 +0530

    controller to delete conatct by ID

--- server/controllers/contact.controller.js
+++ server/controllers/contact.controller.js
@@ -69,7 +69,7 @@ export const getAllContacts = async (req, res) => {
 
 
 
-// ========================= UPDATE CONTACT WITH ID ========================= 
+// ========================= UPDATE CONTACT BY ID ========================= 
 export const updateContact = async (req, res) => {
     try {
         const { id } = req.params;
@@ -80,21 +80,25 @@ export const updateContact = async (req, res) => {
             });
         }
 
+        // updated data from client side
         const updatedData = req.body;
 
+        // find contact by id and update in DB 
         const contact = await ContactModel.findByIdAndUpdate(id,
             updatedData,
             { new: true }
         );
 
+        // if contact not found in DB
         if (!contact) return res.status(404).json({
             succes: true,
             message: 'Contact not found by ID',
         });
 
+        // return success response
         res.status(200).json({
-            succes:true,
-            message:"Conatct updated successfully",
+            succes: true,
+            message: "Conatct updated successfully",
             contact
         });
     } catch (error) {
@@ -102,6 +106,44 @@ export const updateContact = async (req, res) => {
         res.status(500).json({
             succes: false,
             message: 'Error while updating contact by Id',
+            error: error.message
+        });
+    }
+};
+
+
+
+
+// ========================= DELETE CONTACT BY ID ========================= 
+export const deleteContact = async (req, res) => {
+    try {
+        const { id } = req.params;
+        // validate data
+        if (!id) {
+            return res.status(400).json({
+                success: false,
+                message: "conatct ID is required to delete contact",
+            });
+        }
+
+        // find contact by id and delete from DB 
+        const contact = await ContactModel.findByIdAndDelete(id);
+
+        // if contact not found in DB
+        if (!contact) {
+            return res.status(404).json({ message: 'Contact not found by ID' });
+        }   
+
+        // return success response
+        res.status(200).json({
+            success: true,
+            message: 'Contact deleted successfully'
+        });
+    } catch (error) {
+        console.log("Error while deleting contact by ID => ", error)
+        res.status(500).json({
+            succes: false,
+            message: 'Error while deleting contact by ID',
             error: error.message
         });
     }
