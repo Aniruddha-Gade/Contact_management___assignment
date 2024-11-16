bd021b3463641505987157c75dad214d816e05d7
Aniruddha Gade <gadeaniruddha2@gmail.com>
2024-11-16 12:41:16 +0530

    update home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,9 +3,12 @@ import { Link } from "react-router-dom"
 const Home = () => {
     return (
         <div className='flex-center flex-col gap-10 w-full'>
-            <h1 className="text-3xl sm:text-5xl text-green-600 text-center font-bold underline">
-                Contact Management - Mini Feature of a CRM
-            </h1>
+            <div className="text-3xl sm:text-5xl text-green-600 px-10 text-center font-bold">
+                <h1>Contact Management</h1>
+                <p className="text-2xl sm:text-4xl underline">
+                    Mini Feature of a <span className="text-red-500">CRM</span>
+                </p>
+            </div>
 
             <Link to='/dashboard'>
                 <button className='bg-green-500 hover:bg-green-600 p-5 sm:p-7 text-3xl sm:text-5xl font-bold rounded-xl '>
